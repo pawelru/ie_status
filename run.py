8596e93cb9e77364d2a6e57682640b69fb504a63
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2025-01-14 14:04:43 +0100

    reorder by R ver; update labels

--- run.py
+++ run.py
@@ -15,15 +15,25 @@ template = """|
   [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_winbinary&logo=windows&logoColor=white&label=winbinary)](https://pharmaverse.r-universe.dev/{repo_name}/)
   [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_windevel&logo=windows&logoColor=white&label=windevel)](https://pharmaverse.r-universe.dev/{repo_name}/)
   [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_wasmbinary&logo=webassembly&logoColor=white&label=wasmbinary)](https://pharmaverse.r-universe.dev/{repo_name}/)
-| [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B0%5D%5Bstatus%2Ccheck%5D&logo=linux&logoColor=white&label=R4.5.0)](https://pharmaverse.r-universe.dev/{repo_name}/)
-  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B1%5D%5Bstatus%2Ccheck%5D&logo=linux&logoColor=white&label=R4.4.1)](https://pharmaverse.r-universe.dev/{repo_name}/)
-| [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B2%5D%5Bstatus%2Ccheck%5D&logo=apple&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
-  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B3%5D%5Bstatus%2Ccheck%5D&logo=apple&logoColor=white&label=R4.4.1)](https://pharmaverse.r-universe.dev/{repo_name}/)
-| [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B7%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
-  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B8%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.4.1)](https://pharmaverse.r-universe.dev/{repo_name}/)
-| [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B6%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.5.0)](https://pharmaverse.r-universe.dev/{repo_name}/)
-| [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B4%5D%5Bstatus%2Ccheck%5D&logo=webassembly&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
+
+|
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B0%5D%5Bstatus%2Ccheck%5D&logo=linux&logoColor=white&label=R4.5.0)](https://pharmaverse.r-universe.dev/{repo_name}/)
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B1%5D%5Bstatus%2Ccheck%5D&logo=linux&logoColor=white&label=R4.4.2)](https://pharmaverse.r-universe.dev/{repo_name}/)
+
+|
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B3%5D%5Bstatus%2Ccheck%5D&logo=apple&logoColor=white&label=R4.4.2)](https://pharmaverse.r-universe.dev/{repo_name}/)
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B2%5D%5Bstatus%2Ccheck%5D&logo=apple&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
+
+|
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B8%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.4.2)](https://pharmaverse.r-universe.dev/{repo_name}/)
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B7%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
+
+|
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B6%5D%5Bstatus%2Ccheck%5D&logo=windows&logoColor=white&label=R4.5.0)](https://pharmaverse.r-universe.dev/{repo_name}/)
+
+|
   [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B5%5D%5Bstatus%2Ccheck%5D&logo=webassembly&logoColor=white&label=R4.4.1)](https://pharmaverse.r-universe.dev/{repo_name}/)
+  [![](https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fpharmaverse.r-universe.dev%2Fapi%2Fpackages%2F{repo_name}&query=_binaries%5B4%5D%5Bstatus%2Ccheck%5D&logo=webassembly&logoColor=white&label=R4.3.3)](https://pharmaverse.r-universe.dev/{repo_name}/)
 """
 
 no_columns = template.count('|')
